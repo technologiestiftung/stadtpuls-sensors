07f8530ceed937c399902995072e2a5e6db66562
Lucas Vogel <contact@vogelino.com>
2021-11-15 16:09:11 +0100

    update ttn script with os_runloop

--- audio-ttn/src/main.cpp
+++ audio-ttn/src/main.cpp
@@ -42,7 +42,6 @@
 // our secrets
 #include "env.h"
 
-int sendingPeriod = 10 * 1000;
 int sendingIteration = 0;
 int measuringPeriod = 100;
 int measuringIteration = 0;
@@ -78,8 +77,71 @@ void os_getDevKey(u1_t *buf) {}
 uint8_t tx_payload[8];
 static osjob_t sendjob;
 
+void updateValues()
+{
+  valCurrentIteration = analogRead(SENSOR_PIN);
+
+  if (minPeak == 0)
+  {
+    minPeak = valCurrentIteration;
+  }
+  if (maxPeak == 0)
+  {
+    maxPeak = valCurrentIteration;
+  }
+  if (valCurrentIteration > maxPeak)
+  {
+    maxPeak = valCurrentIteration;
+  }
+  if (valCurrentIteration < minPeak)
+  {
+    minPeak = valCurrentIteration;
+  }
+  if (valPrevIteration > valTwoIterationsAgo && valPrevIteration > valCurrentIteration)
+  {
+    sumAllHighPeakValues += valPrevIteration;
+    totalHighPeaksCount += 1;
+  }
+  if (valPrevIteration < valTwoIterationsAgo && valPrevIteration < valCurrentIteration)
+  {
+    sumAllLowPeakValues += valPrevIteration;
+    totalLowPeaksCount += 1;
+  }
+
+  valTwoIterationsAgo = valPrevIteration;
+  valPrevIteration = valCurrentIteration;
+}
+
+void updateAverages()
+{
+  if (totalHighPeaksCount > 0)
+  {
+    averageHighPeaks = sumAllHighPeakValues / totalHighPeaksCount;
+  }
+  if (totalLowPeaksCount > 0)
+  {
+    averageLowPeaks = sumAllLowPeakValues / totalLowPeaksCount;
+  }
+  if (totalHighPeaksCount > 0 && totalLowPeaksCount > 0)
+  {
+    totalAverage = averageLowPeaks + ((averageHighPeaks - averageLowPeaks) / 2);
+  }
+}
+
+void resetValues()
+{
+  sumAllHighPeakValues = 0;
+  totalHighPeaksCount = 0;
+  sumAllLowPeakValues = 0;
+  totalLowPeaksCount = 0;
+  maxPeak = 0;
+  minPeak = 0;
+  averageHighPeaks = 0;
+  averageLowPeaks = 0;
+}
+
 // Schedule TX every this many seconds (might become longer due to duty cycle
-// limitations).
+// limitations).ssss
 const unsigned TX_INTERVAL = 60 * 5; // 5 minutes
 
 //
@@ -144,6 +206,7 @@ void onEvent(ev_t ev) {
     break;
   case EV_TXCOMPLETE:
     Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+    resetValues();
     if (LMIC.txrxFlags & TXRX_ACK)
       Serial.println(F("Received ack"));
     if (LMIC.dataLen) {
@@ -204,7 +267,6 @@ void onEvent(ev_t ev) {
 // needed
 //
 void generate_payload() {
-  
   tx_payload[0] = (int)(averageHighPeaks - baseADCVal) >> 8;
   tx_payload[1] = (int)(averageHighPeaks - baseADCVal);
   tx_payload[2] = (int)(maxPeak - baseADCVal) >> 8;
@@ -226,6 +288,11 @@ void do_send(osjob_t *j) {
     Serial.println(F("OP_TXRXPEND, not sending"));
   } else {
 
+    Serial.println("\n–––––––––––––––––––––––––––––––");
+    Serial.println(String("Sending Iteration: " + String(sendingIteration)));
+    Serial.println("- - - - - - - - - - - - - - - -\n");
+
+    updateAverages();
     generate_payload();
 
     Serial.print("Payload: ");
@@ -340,86 +407,9 @@ void setup() {
   do_send(&sendjob);
 }
 
-void updateValues()
-{
-  valCurrentIteration = analogRead(SENSOR_PIN);
-
-  if (minPeak == 0)
-  {
-    minPeak = valCurrentIteration;
-  }
-  if (maxPeak == 0)
-  {
-    maxPeak = valCurrentIteration;
-  }
-  if (valCurrentIteration > maxPeak)
-  {
-    maxPeak = valCurrentIteration;
-  }
-  if (valCurrentIteration < minPeak)
-  {
-    minPeak = valCurrentIteration;
-  }
-  if (valPrevIteration > valTwoIterationsAgo && valPrevIteration > valCurrentIteration)
-  {
-    sumAllHighPeakValues += valPrevIteration;
-    totalHighPeaksCount += 1;
-  }
-  if (valPrevIteration < valTwoIterationsAgo && valPrevIteration < valCurrentIteration)
-  {
-    sumAllLowPeakValues += valPrevIteration;
-    totalLowPeaksCount += 1;
-  }
-
-  valTwoIterationsAgo = valPrevIteration;
-  valPrevIteration = valCurrentIteration;
-}
-
-void updateAverages()
-{
-  if (totalHighPeaksCount > 0)
-  {
-    averageHighPeaks = sumAllHighPeakValues / totalHighPeaksCount;
-  }
-  if (totalLowPeaksCount > 0)
-  {
-    averageLowPeaks = sumAllLowPeakValues / totalLowPeaksCount;
-  }
-  if (totalHighPeaksCount > 0 && totalLowPeaksCount > 0)
-  {
-    totalAverage = averageLowPeaks + ((averageHighPeaks - averageLowPeaks) / 2);
-  }
-}
-
-void resetValues()
-{
-  sumAllHighPeakValues = 0;
-  totalHighPeaksCount = 0;
-  sumAllLowPeakValues = 0;
-  totalLowPeaksCount = 0;
-  maxPeak = 0;
-  minPeak = 0;
-  averageHighPeaks = 0;
-  averageLowPeaks = 0;
-}
-
 void loop()
 {
-  if (millis() >= sendingIteration * sendingPeriod)
-  {
-    Serial.println("\n–––––––––––––––––––––––––––––––");
-    Serial.println(String("Sending Iteration: " + String(sendingIteration)));
-    Serial.println("- - - - - - - - - - - - - - - -\n");
-
-    updateAverages();
-    sendingIteration += 1;
-
-    if (sendingIteration > 1)
-    {
-      os_runloop_once(); // TTN
-      resetValues();
-    }
-  }
+  os_runloop_once();
 
   if (millis() >= measuringIteration * measuringPeriod)
   {
