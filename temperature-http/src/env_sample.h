e47c107b89aad26e5ae0367d5733d50ce365d5dc
ff6347 <hash@ff6347.email>
2022-06-04 21:36:57 +0200

    ci: fix redefinition warning

--- temperature-http/src/env_sample.h
+++ temperature-http/src/env_sample.h
@@ -4,7 +4,8 @@ const char *server = "example.com";
 const char *auth_token = ""; // get it from stadtpuls.com
 
 // we put these into a different file for mass flashing
+// see util/sensorsenv_<NAME>.h
 // you can keep them here
-char sensor_id[] = "100000"; // get it from stadtpuls.com
-char sensor_name[] = "Cloak";
-String path = "/api/v3/sensors/" + String(sensor_id) + "/records";
+// char sensor_id[] = "100000"; // get it from stadtpuls.com
+// char sensor_name[] = "Cloak";
+// String path = "/api/v3/sensors/" + String(sensor_id) + "/records";
