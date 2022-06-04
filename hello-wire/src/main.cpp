9617288ea035a5756623193f4eb49069fabf59ef
ff6347 <hash@ff6347.email>
2022-06-04 21:02:14 +0200

    another wire scanner

--- hello-wire/src/main.cpp
+++ hello-wire/src/main.cpp
@@ -1,10 +1,14 @@
 #include <Arduino.h>
 #include <Wire.h>
-#define I2C_SDA_PIN 23
-#define I2C_SCL_PIN 22
+#include <heltec.h>
+#define I2C_SDA_PIN 4
+#define I2C_SCL_PIN 15
+TwoWire I2Cone = TwoWire(0);
 void setup()
 {
-  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  Heltec.begin(true, false, true);
+  I2Cone.begin(I2C_SDA_PIN, I2C_SCL_PIN);
+  // Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
 
   Serial.begin(115200);
   while (!Serial)
@@ -22,12 +26,12 @@ void loop()
   nDevices = 0;
   for (address = 1; address < 127; address++)
   {
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
+    I2Cone.beginTransmission(address);
+    error = I2Cone.endTransmission();
 
-    Wire.beginTransmission(address + 1);
+    I2Cone.beginTransmission(address + 1);
 
-    if (error == 0 && Wire.endTransmission() != 0) // Special flag for SAMD Series
+    if (error == 0 && I2Cone.endTransmission() != 0) // Special flag for SAMD Series
     {
       Serial.print("I2C device found at address 0x");
       if (address < 16)
