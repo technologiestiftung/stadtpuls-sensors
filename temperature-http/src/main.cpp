57059a3921c13f61404c46ccd3002bd9573376ac
ff6347 <hash@ff6347.email>
2022-06-23 12:58:49 +0200

    feat: Add root ca cert and info
    Allows secure connection

--- temperature-http/src/main.cpp
+++ temperature-http/src/main.cpp
@@ -60,6 +60,50 @@
 // #include "util/sensors/env_tigra.h"
 // #include "util/sensors/env_wasp.h"
 
+// to make sure you are sending data to api.stadtpuls.com you need the following root certificate
+// to get this via your browser do the folowing
+// - open the api url of your sensor in Firefox e.g. https://api.stadtpuls.com/api/v1/sensors/<sensor_id>/records
+// - click on the little lock icon left from the URL in the address bar
+// - click on the "connection secure" button
+// - click on the "More information" button
+// - click on the "View certificate" button
+// - go to the second tab where you can see infos about the root ca
+// - scroll down and downlad the PEM (cert)
+// - save the PEM to a file called root-ca.pem
+// - open the file in your editor and copy the content to the clipboard
+// - insert it into your editor and adjust it like the cert below.
+// Hint!: the new line characters are important for the cert to work correctly
+
+const char *root_ca =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIEmDCCA4CgAwIBAgIQAtiaFXPzs7jaFFtYOJgpBzANBgkqhkiG9w0BAQsFADBa\n"
+    "MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl\n"
+    "clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw\n"
+    "MDEyNzEyNDYzOVoXDTI0MTIzMTIzNTk1OVowSjELMAkGA1UEBhMCVVMxGTAXBgNV\n"
+    "BAoTEENsb3VkZmxhcmUsIEluYy4xIDAeBgNVBAMTF0Nsb3VkZmxhcmUgSW5jIFJT\n"
+    "QSBDQS0yMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtV0myAmrsTNd\n"
+    "DLKWBcIxA05+X4cCxm+AJO3arhy+552A98XPVAFmOCjo5mm7nfiBMWpW9ejBpehZ\n"
+    "6LPgOgFhCCiwfs2g/c43AMPpqY0LyXjKpmBGgK90BS1P2ugJUsibWjLPISFHj1wa\n"
+    "iqoXwBhwsQHx3t8thamV7FtXOTm+hcLv17YsSj/R1rMhopdPJSxCI33XsxnWEmmF\n"
+    "AAKv1aoS2Ghkf+OtGJ1SL8Xl9JGv5QnZi0bnzzIVIvA3xP2L5uIdFsw6SNeBYYK/\n"
+    "PXLZcF7sCmkk4jxI4/7T5VHUK8ASoU+TcJm/t9KDbmktyM2iYhhMQzdfFjp2k1r6\n"
+    "bmldmsWIwQIDAQABo4IBaDCCAWQwHQYDVR0OBBYEFBipGvyyRUnBbzA0CCvZh5yw\n"
+    "JVd6MB8GA1UdIwQYMBaAFOWdWTCCR1jMrPoIVDaGezq1BE3wMA4GA1UdDwEB/wQE\n"
+    "AwIBhjAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwEgYDVR0TAQH/BAgw\n"
+    "BgEB/wIBADA0BggrBgEFBQcBAQQoMCYwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3Nw\n"
+    "LmRpZ2ljZXJ0LmNvbTA6BgNVHR8EMzAxMC+gLaArhilodHRwOi8vY3JsMy5kaWdp\n"
+    "Y2VydC5jb20vT21uaXJvb3QyMDI1LmNybDBtBgNVHSAEZjBkMDcGCWCGSAGG/WwB\n"
+    "ATAqMCgGCCsGAQUFBwIBFhxodHRwczovL3d3dy5kaWdpY2VydC5jb20vQ1BTMAsG\n"
+    "CWCGSAGG/WwBAjAIBgZngQwBAgEwCAYGZ4EMAQICMAgGBmeBDAECAzANBgkqhkiG\n"
+    "9w0BAQsFAAOCAQEAfLCdjh6mZHIQ4WHd83Q9UKca58JOFwwVJisXj0HnkPCw8/LS\n"
+    "51CA0gfGqX+6k+u+wcGG+E+EvAV85nIv6bbGdmnd8mpHa5NUGKQNgAM1J92Kn8iE\n"
+    "l9O04NqmBfDnz+admJTSy9oid9hJC6hVjonU0s7Q6NrgQv0dzn6WhMqnw5Ennym8\n"
+    "/+0uNP1GKu9OVnzo3CKX7VMJW7p74PJPpVmVQc2zci5cb3pKFUMrIsvUPyB88/p8\n"
+    "2x4PSnNx6dXdRraeHRMegHGZ3/VQfjOB0jV1JF6PmFqokkX0tNiIprFkBun7+WVI\n"
+    "9pbDH55GEgF+WjBTXTQcFeIS8vGLlfCU+K3jkw==\n"
+    "-----END CERTIFICATE-----\n";
+const char *server = "api.stadtpuls.com";
+
 //  ██████╗ ██╗     ███████╗██████╗
 // ██╔═══██╗██║     ██╔════╝██╔══██╗
 // ██║   ██║██║     █████╗  ██║  ██║
